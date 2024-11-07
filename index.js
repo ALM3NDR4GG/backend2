eda338f6a6e8242798e92e83f69fbe6457653b4a
ALM3NDR4GG <alexis.priego.almendra@gmail.com>
2024-11-07 16:57:31 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.put('/api/notes/:id',(request,response) => {
     response.json(updateNote)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     //console.log( `Server Express Running On Port ${PORT}`)
 })
