e0476bc483749fb713c5d68141bb653845dd5b9d
ALM3NDR4GG <alexis.priego.almendra@gmail.com>
2024-11-07 18:07:29 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors')
-const app = express()
+const app=express()
+
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
@@ -15,33 +16,32 @@ const requestLogger=(request, response, next) =>{
 
 app.use(requestLogger)
 
-
 let notes = [
     {
-        id:1,
-        content: 'HTML Is Easy',
+        id: 1,
+        content: 'HTML is easy',
         important: true
     },
     {
-        id:2,
-        content: 'Browser Can Execute Only JavaScript',
+        id: 2,
+        content: 'Browser can execute only Javascript',
         important: false
     },
     {
-        id:3,
-        content: 'Get And Post Are The Most Important Methods Of HTTP Protocol',
+        id: 3,
+        content: 'GET and POST are the most important methods of HTTP Protocol',
         important: true
-    }
+    },
 ]
 
-app.get('/',(reques,response)=>{
-    response.send('<h1>API REST FROM NOTES<h1/>')
+app.get('/',(request,response) => {
+    response.send('<h1>API REST FROM NOTES</h1>')
 })
-
-app.get('/api/notes',(request,response)=>{
+//Regresa todos
+app.get('/api/notes',(request,response) => {
     response.json(notes)
 })
-
+//Buscar uno solo
 app.get('/api/notes/:id',(request,response) => {
     const id = Number(request.params.id)
     //console.log('id:', id);
@@ -54,10 +54,10 @@ app.get('/api/notes/:id',(request,response) => {
         response.status(404).end()
     }
 })
-
-app.delete('/api/notes/:id',(request,response)=>{
-    const id= Number(request.params.id)
-   // console.log('id:',id);
+//Borrar uno
+app.delete('/api/notes/:id',(request,response) => {
+    const id = Number(request.params.id)
+    //console.log('Delete id:', id);
     notes=notes.filter(n => n.id !== id)
     response.status(204).end()
 })
@@ -69,15 +69,16 @@ const generateId = () => {
     return maxId+1
 }
 
-app.post('/api/notes',(request,response)=>{
+//Agregar uno
+app.post('/api/notes',(request,response) => {
     const body = request.body
-    if(!body.content){
+    if (!body.content) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
     const note = {
-        id: generatedId(),
+        id: generateId(),
         content: body.content,
         important: Boolean(body.important) || false
     }
@@ -95,7 +96,7 @@ app.put('/api/notes/:id',(request,response) => {
     response.json(updateNote)
 })
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, ()=>{
-    //console.log( `Server Express Running On Port ${PORT}`)
+const PORT= process.env.PORT || 3001
+app.listen(PORT, ()=> {
+    //console.log(Server express running on port ${PORT});
 })
